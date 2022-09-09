7e907cfded70e5bce0aba1484aeff735ec760a00
Li Cheng <lilybearcheng@Lis-MacBook-Pro.home>
2022-09-09 01:14:00 -0700

    added two button components

--- src/App.js
+++ src/App.js
@@ -1,23 +1,23 @@
-import logo from './logo.svg';
 import './App.css';
+import { ethers } from 'ethers';
+import { ConnectButton } from './ConnectButton';
+import { ContractButton } from './ContractButton';
+
+
 
 function App() {
+  
+  // const connect = async () => {
+  //   const provider = new ethers.providers.Web3Provider(window.ethereum);
+  //   // await provider.send('eth_requestAccounts', []);
+  //   const signer = provider.getSigner(); // use signer to send eth
+  // }
+  
+  
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className='buttons'>
+      <ConnectButton />
+      <ContractButton />
     </div>
   );
 }

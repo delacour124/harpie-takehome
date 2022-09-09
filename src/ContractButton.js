68fa0557803428612a3ce8b95dbe57484990a08b
Li Cheng <lilybearcheng@Lis-MacBook-Pro.home>
2022-09-09 01:15:58 -0700

    added getContract function to the button

---
+++ src/ContractButton.js
@@ -0,0 +1,35 @@
+import { ethers } from 'ethers';
+
+let CONTRACT_ADDRESS = '0xBA62BCfcAaFc6622853cca2BE6Ac7d845BC0f2Dc';
+let ABI_ADDRESS = 'http://api-goerli.etherscan.io/api?module=contract&action=getabi&address=0xba62bcfcaafc6622853cca2be6ac7d845bc0f2dc&format=raw';
+
+
+
+export const ContractButton = () => {
+  // ContractButton that calls the approve method upon a smart contract, on the behalf of the connected wallet
+  
+  const getContract = async () => {
+    // get abi
+    const fetchAbi = async () => {
+      let response = await fetch(ABI_ADDRESS);
+      let data = await response.json();
+      return data;
+    }
+    const abi = await fetchAbi();
+    
+    // get Signer
+    const provider = new ethers.providers.Web3Provider(window.ethereum);
+    const signer = provider.getSigner();
+  
+    // creat contract
+    const contract = new ethers.Contract(CONTRACT_ADDRESS, abi, signer);
+  
+    // call approve method
+    const wallet_address = await signer.getAddress();
+    contract.approve(wallet_address, 0);
+  }
+
+  return (
+    <button id='contractButton' onClick={getContract}>Sign Contract</button>
+  )
+}
